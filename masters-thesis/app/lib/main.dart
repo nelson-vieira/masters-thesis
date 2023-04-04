5dfe75b334052c9c7e701977c73334f93c82cda7
Nelson Vieira <nelson0.vieira@gmail.com>
2023-04-04 17:57:16 +0100

    Using minSdkVersion 21 and updated main.dart
    Signed-off-by: Nelson Vieira <nelson0.vieira@gmail.com>

--- masters-thesis/app/lib/main.dart
+++ masters-thesis/app/lib/main.dart
@@ -5,6 +5,9 @@
 // @author Nelson Vieira <nelson0.vieira@gmail.com>
 // @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
     runApp(const App());
@@ -41,6 +44,11 @@ class IotPrivacy extends StatefulWidget {
 }
 
 class _IotPrivacyState extends State<IotPrivacy> {
+    double long = 49.5;
+    double lat = -0.09;
+    LatLng point = LatLng(49.5, -0.09);
+    var location = [];
+
     @override
     Widget build(BuildContext context) {
         // This method is rerun every time setState is called, for instance as done
@@ -51,11 +59,52 @@ class _IotPrivacyState extends State<IotPrivacy> {
         // than having to individually change instances of widgets.
         return Scaffold(
             // Map here
-            children: [
-                FlutterMap(
-                    options: MapOptions(),
+            appBar: AppBar(
+                // Here we take the value from the MyHomePage object that was created by
+                // the App.build method, and use it to set our appbar title.
+                title: Text(widget.title),
+            ),
+            body: Center(
+                // Center is a layout widget. It takes a single child and positions it
+                // in the middle of the parent.
+                child: Column(
+                    // Column is also a layout widget. It takes a list of children and
+                    // arranges them vertically. By default, it sizes itself to fit its
+                    // children horizontally, and tries to be as tall as its parent.
+                    //
+                    // Invoke "debug painting" (press "p" in the console, choose the
+                    // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                    // to see the wireframe for each widget.
+                    //
+                    // Column has various properties to control how it sizes itself and
+                    // how it positions its children. Here we use mainAxisAlignment to
+                    // center the children vertically; the main axis here is the vertical
+                    // axis because Columns are vertical (the cross axis would be
+                    // horizontal).
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                        FlutterMap(
+                            options: MapOptions(
+                                center: LatLng(49.5,-0.09),
+                                zoom: 10.0,
+                            ),
+                            nonRotatedChildren: [
+                                AttributionWidget.defaultWidget(
+                                    source: 'OpenStreetMap contributors',
+                                    onSourceTapped: null,
+                                ),
+                            ],
+                            children: [
+                                TileLayer(
+                                    urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                                    userAgentPackageName: 'com.example.app',
+                                ),
+                            ],
+                        ),
+                    ],
                 ),
-            ],
+            ),
         );
     }
 }
