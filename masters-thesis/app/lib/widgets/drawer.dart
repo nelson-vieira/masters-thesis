d1f400f4ca7567b98fc642b1312851e0221f6ae3
Nelson Vieira <nelson0.vieira@gmail.com>
2023-04-16 18:18:51 +0100

    Added missing copyright notices to application source files
    Signed-off-by: Nelson Vieira <nelson0.vieira@gmail.com>

--- masters-thesis/app/lib/widgets/drawer.dart
+++ masters-thesis/app/lib/widgets/drawer.dart
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (c) 2023 Nelson Vieira
+//
+// @author Nelson Vieira <nelson0.vieira@gmail.com>
+// @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
 import "package:flutter/material.dart";
 import "package:app/pages/home.dart";
 import "package:app/pages/about.dart";
