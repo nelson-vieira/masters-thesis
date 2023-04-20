81222a66da2305e19d24dcd4cc050316ad00a44a
Nelson Vieira <2080511@student.uma.pt>
2023-04-20 20:21:01 +0100

    Updated application encyclopedia page
    Signed-off-by: Nelson Vieira <2080511@student.uma.pt>

--- masters-thesis/app/lib/pages/encyclopedia.dart
+++ masters-thesis/app/lib/pages/encyclopedia.dart
@@ -9,6 +9,7 @@ import "dart:io";
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
 import "package:latlong2/latlong.dart";
+import "package:ipsum/ipsum.dart";
 import 'package:app/pages/home.dart';
 import 'package:app/pages/about.dart';
 import 'package:app/pages/account.dart';
@@ -20,6 +21,8 @@ class Encyclopedia extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Ipsum lip = Ipsum();
+    String sentences = lip.sentences(1);
     return Scaffold(
       appBar: AppBar(
           leading: IconButton(
@@ -38,26 +41,209 @@ class Encyclopedia extends StatelessWidget {
             const EdgeInsets.only(top: 2.0, right: 4.0, left: 4.0, bottom: 4.0),
         child: Container(
           padding: EdgeInsets.all(35.0),
+          color: Color.fromARGB(255, 18, 180, 132),
           child: Column(
             children: [
-              Center(
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 50.0),
-                  child: Image.asset("assets/icon.png", width: 120.0),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: Text(
+                            "Want to learn more about privacy in the Internet of Things? You have come to the right place, below you can learn how you to better protect your privacy and also learn new terms.",
+                            locale: Locale("en", "UK"),
+                            textAlign: TextAlign.left,
+                            overflow: TextOverflow.visible,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 245, 245, 245),
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: const Text(
+                                        "Item 1",
+                                        locale: Locale("en", "UK"),
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 245, 245, 245),
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    sentences,
+                                    locale: Locale("en", "UK"),
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 245, 245, 245),
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: const Text(
+                                        "Item 2",
+                                        locale: Locale("en", "UK"),
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 245, 245, 245),
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    sentences,
+                                    locale: Locale("en", "UK"),
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 245, 245, 245),
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: const Text(
+                                        "Item 3",
+                                        locale: Locale("en", "UK"),
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 245, 245, 245),
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    sentences,
+                                    locale: Locale("en", "UK"),
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 245, 245, 245),
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: const Text(
+                                        "Item 4",
+                                        locale: Locale("en", "UK"),
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 245, 245, 245),
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    sentences,
+                                    locale: Locale("en", "UK"),
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 245, 245, 245),
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: const Text(
+                                        "Item 5",
+                                        locale: Locale("en", "UK"),
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 245, 245, 245),
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    sentences,
+                                    locale: Locale("en", "UK"),
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 245, 245, 245),
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "About",
-                  locale: Locale("en", "UK"),
-                  textAlign: TextAlign.left,
-                  overflow: TextOverflow.visible,
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 245, 245, 245),
-                      fontSize: 16.0),
-                ),
-              )
             ],
           ),
         ),
