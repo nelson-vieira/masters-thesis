2da42ef0447c0c6575261f6c4575c23d87f060dc
Nelson Vieira <2080511@student.uma.pt>
2023-05-24 22:01:46 +0100

    Create verify email page
    Signed-off-by: Nelson Vieira <2080511@student.uma.pt>

---
+++ masters-thesis/app/lib/pages/auth/verify_email.dart
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (c) 2023 Nelson Vieira
+//
+// @author Nelson Vieira <2080511@student.uma.pt>
+// @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
+import "package:email_validator/email_validator.dart";
+import "package:flutter/material.dart";
+import "package:flutter_map/flutter_map.dart";
+import "package:latlong2/latlong.dart";
+import "package:firebase_core/firebase_core.dart";
+import "package:firebase_core_platform_interface/firebase_core_platform_interface.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:datetime_picker_formfield/datetime_picker_formfield.dart";
+import "package:intl/intl.dart";
+import "package:flutter/gestures.dart";
+import "package:app/main.dart";
+import "package:app/models/user.dart";
+import 'package:app/pages/public/home.dart';
+import 'package:app/pages/public/about.dart';
+import 'package:app/pages/public/encyclopedia.dart';
+import 'package:app/pages/auth/account.dart';
+import 'package:app/pages/devices/create.dart';
+import 'package:app/pages/devices/update.dart';
+import 'package:app/pages/devices/devices.dart';
+import 'package:app/pages/auth/auth.dart';
+import 'package:app/helpers/app.dart';
+
+class VerifyEmail extends StatefulWidget {
+  static const String route = "/verify";
+
+  const VerifyEmail({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<VerifyEmail> createState() => _VerifyEmailState();
+}
+
+class _VerifyEmailState extends State<VerifyEmail> {
+  bool isEmailVerified = false;
+
+  @override
+  void iniState() {
+    super.initState();
+
+    isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+
+    if (!isEmailVerified) {
+      sendVerificationEmail();
+    }
+  }
+
+  Future sendVerificationEmail() async {
+    final user = FirebaseAuth.instance.currentUser!;
+    await user.sendEmailVerification();
+  }
+
+  @override
+  Widget build(BuildContext context) => isEmailVerified
+      ? Account()
+      : Scaffold(
+          appBar: AppBar(
+          title: Text("Verify Email"),
+        ));
+}
