3c16783be35ebc43e2a944bda2b7ba037170be53
Nelson Vieira <2080511@student.uma.pt>
2023-05-25 16:08:08 +0100

    Create category model class
    Signed-off-by: Nelson Vieira <2080511@student.uma.pt>

---
+++ masters-thesis/app/lib/models/category.dart
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (c) 2023 Nelson Vieira
+//
+// @author Nelson Vieira <2080511@student.uma.pt>
+// @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
+import 'package:app/helpers/app.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class Category {
+  String id;
+  // Name of the category
+  final String name;
+  // Metadata about this device on the database
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Category({
+    this.id = "",
+    required this.name,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  // This function serves to send the data about this device to Firebase
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "createdAt": createdAt,
+        "updatedAt": updatedAt,
+      };
+
+  // This function serves to import the data on Firebase to be shown on the app
+  static Category fromJson(Map<String, dynamic> json) => Category(
+        id: Helper.transformString(json["id"]),
+        name: Helper.transformString(json["name"]),
+        createdAt: Helper.toTimestamp(json["createdAt"]).toDate(),
+        updatedAt: Helper.toTimestamp(json["updatedAt"]).toDate(),
+      );
+}
