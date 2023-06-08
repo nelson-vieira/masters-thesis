6a9d8ca4085c460efe954e63f3543597887489fc
Nelson Vieira <2080511@student.uma.pt>
2023-06-08 23:13:04 +0100

    Update information page
    Signed-off-by: Nelson Vieira <2080511@student.uma.pt>

--- masters-thesis/app/lib/pages/public/encyclopedia.dart
+++ masters-thesis/app/lib/pages/public/encyclopedia.dart
@@ -4,12 +4,11 @@
 //
 // @author Nelson Vieira <nelson0.vieira@gmail.com>
 // @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
-import "dart:io";
-
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
 import "package:latlong2/latlong.dart";
 import "package:ipsum/ipsum.dart";
+import "package:firebase_auth/firebase_auth.dart";
 import "package:app/main.dart";
 import 'package:app/pages/public/home.dart';
 import 'package:app/pages/public/about.dart';
@@ -24,13 +23,11 @@ class Encyclopedia extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Ipsum lip = Ipsum();
-    String sentences = lip.sentences(1);
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Encyclopedia",
-          style: const TextStyle(fontSize: 30),
+        title: const Text(
+          "More Information",
+          style: TextStyle(fontSize: 30),
         ),
         backgroundColor: const Color(0xFF334150),
       ),
@@ -38,7 +35,7 @@ class Encyclopedia extends StatelessWidget {
         padding:
             const EdgeInsets.only(top: 2.0, right: 4.0, left: 4.0, bottom: 4.0),
         child: Container(
-          padding: EdgeInsets.all(35.0),
+          padding: const EdgeInsets.all(35.0),
           color: const Color.fromARGB(255, 16, 44, 53),
           child: Column(
             children: [
@@ -46,9 +43,9 @@ class Encyclopedia extends StatelessWidget {
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      Center(
+                      const Center(
                         child: Padding(
-                          padding: const EdgeInsets.only(bottom: 20.0),
+                          padding: EdgeInsets.only(bottom: 20.0),
                           child: Text(
                             "Want to learn more about privacy in the Internet of Things? You have come to the right place, below you can learn how you to better protect your privacy and also learn new terms.",
                             locale: Locale("en", "UK"),
@@ -61,20 +58,56 @@ class Encyclopedia extends StatelessWidget {
                           ),
                         ),
                       ),
-                      Align(
+                      FirebaseAuth.instance.currentUser != null
+                          ? const Padding(
+                              padding: EdgeInsets.only(bottom: 20.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(bottom: 8.0),
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "How to add a device?",
+                                        locale: Locale("en", "UK"),
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 245, 245, 245),
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    "You can insert a device by insert the device data: the name of the device, category of the device, what is the purpose of data collection, who has access to the data, for how long is the data stored, can the data identify an individual, what is currently being done with the data collected, privacy options of the device, device coordinates, and who is the device owner (in the case of an organization).",
+                                    locale: Locale("en", "UK"),
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.visible,
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 245, 245, 245),
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : Container(),
+                      const Align(
                         alignment: Alignment.topLeft,
                         child: Column(
                           children: [
                             Padding(
-                              padding: const EdgeInsets.only(bottom: 20.0),
+                              padding: EdgeInsets.only(bottom: 20.0),
                               child: Column(
                                 children: [
                                   Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    padding: EdgeInsets.only(bottom: 8.0),
                                     child: Align(
                                       alignment: Alignment.center,
-                                      child: const Text(
-                                        "Item 1",
+                                      child: Text(
+                                        "What is Digital Privacy?",
                                         locale: Locale("en", "UK"),
                                         overflow: TextOverflow.visible,
                                         style: TextStyle(
@@ -87,7 +120,7 @@ class Encyclopedia extends StatelessWidget {
                                     ),
                                   ),
                                   Text(
-                                    sentences,
+                                    "Digital privacy is the right of the individual to govern how personal information is collected, stored, and used, it frequently involves handling sensitive information with care, and as such, organizations must be open and honest about the kind of data they plan to gather, why they need it, and where and with whom they plan to share it. Users should have the right to control their shared information.",
                                     locale: Locale("en", "UK"),
                                     textAlign: TextAlign.left,
                                     overflow: TextOverflow.visible,
@@ -100,15 +133,15 @@ class Encyclopedia extends StatelessWidget {
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(bottom: 20.0),
+                              padding: EdgeInsets.only(bottom: 20.0),
                               child: Column(
                                 children: [
                                   Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    padding: EdgeInsets.only(bottom: 8.0),
                                     child: Align(
                                       alignment: Alignment.center,
-                                      child: const Text(
-                                        "Item 2",
+                                      child: Text(
+                                        "What is Internet of Things?",
                                         locale: Locale("en", "UK"),
                                         overflow: TextOverflow.visible,
                                         style: TextStyle(
@@ -121,7 +154,7 @@ class Encyclopedia extends StatelessWidget {
                                     ),
                                   ),
                                   Text(
-                                    sentences,
+                                    "An open and comprehensive network of intelligent objects that have the capacity to auto-organize, share information, data and resources, reacting and acting in face of situations and changes in the environment.",
                                     locale: Locale("en", "UK"),
                                     textAlign: TextAlign.left,
                                     overflow: TextOverflow.visible,
@@ -134,15 +167,15 @@ class Encyclopedia extends StatelessWidget {
                               ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(bottom: 20.0),
+                              padding: EdgeInsets.only(bottom: 20.0),
                               child: Column(
                                 children: [
                                   Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    padding: EdgeInsets.only(bottom: 8.0),
                                     child: Align(
                                       alignment: Alignment.center,
-                                      child: const Text(
-                                        "Item 3",
+                                      child: Text(
+                                        "What type of data can IoT devices collect?",
                                         locale: Locale("en", "UK"),
                                         overflow: TextOverflow.visible,
                                         style: TextStyle(
@@ -155,7 +188,7 @@ class Encyclopedia extends StatelessWidget {
                                     ),
                                   ),
                                   Text(
-                                    sentences,
+                                    "Internet of Things devices can collect visual, audio, presence, location, biometrics, environment and unique identification data.",
                                     locale: Locale("en", "UK"),
                                     textAlign: TextAlign.left,
                                     overflow: TextOverflow.visible,
@@ -167,74 +200,40 @@ class Encyclopedia extends StatelessWidget {
                                 ],
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 20.0),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Align(
-                                      alignment: Alignment.center,
-                                      child: const Text(
-                                        "Item 4",
-                                        locale: Locale("en", "UK"),
-                                        overflow: TextOverflow.visible,
-                                        style: TextStyle(
-                                          color: Color.fromARGB(
-                                              255, 245, 245, 245),
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    sentences,
-                                    locale: Locale("en", "UK"),
-                                    textAlign: TextAlign.left,
-                                    overflow: TextOverflow.visible,
-                                    style: TextStyle(
-                                      color: Color.fromARGB(255, 245, 245, 245),
-                                      fontSize: 16.0,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 20.0),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Align(
-                                      alignment: Alignment.center,
-                                      child: const Text(
-                                        "Item 5",
-                                        locale: Locale("en", "UK"),
-                                        overflow: TextOverflow.visible,
-                                        style: TextStyle(
-                                          color: Color.fromARGB(
-                                              255, 245, 245, 245),
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    sentences,
-                                    locale: Locale("en", "UK"),
-                                    textAlign: TextAlign.left,
-                                    overflow: TextOverflow.visible,
-                                    style: TextStyle(
-                                      color: Color.fromARGB(255, 245, 245, 245),
-                                      fontSize: 16.0,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                            // Padding(
+                            //   padding: EdgeInsets.only(bottom: 20.0),
+                            //   child: Column(
+                            //     children: [
+                            //       Padding(
+                            //         padding: EdgeInsets.only(bottom: 8.0),
+                            //         child: Align(
+                            //           alignment: Alignment.center,
+                            //           child: Text(
+                            //             "Item 5",
+                            //             locale: Locale("en", "UK"),
+                            //             overflow: TextOverflow.visible,
+                            //             style: TextStyle(
+                            //               color: Color.fromARGB(
+                            //                   255, 245, 245, 245),
+                            //               fontSize: 18.0,
+                            //               fontWeight: FontWeight.bold,
+                            //             ),
+                            //           ),
+                            //         ),
+                            //       ),
+                            //       Text(
+                            //         "Something...",
+                            //         locale: Locale("en", "UK"),
+                            //         textAlign: TextAlign.left,
+                            //         overflow: TextOverflow.visible,
+                            //         style: TextStyle(
+                            //           color: Color.fromARGB(255, 245, 245, 245),
+                            //           fontSize: 16.0,
+                            //         ),
+                            //       ),
+                            //     ],
+                            //   ),
+                            // ),
                           ],
                         ),
                       ),
