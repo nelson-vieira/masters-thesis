24cf5555c0311393375a54a75eb65f7cbdd5e17a
Nelson Vieira <2080511@student.uma.pt>
2023-06-08 22:43:27 +0100

    Update about page
    Signed-off-by: Nelson Vieira <2080511@student.uma.pt>

--- masters-thesis/app/lib/pages/public/about.dart
+++ masters-thesis/app/lib/pages/public/about.dart
@@ -4,8 +4,6 @@
 //
 // @author Nelson Vieira <nelson0.vieira@gmail.com>
 // @license AGPL-3.0 <https://www.gnu.org/licenses/agpl-3.0.txt>
-import "dart:io";
-
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
 import "package:latlong2/latlong.dart";
@@ -24,13 +22,11 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Ipsum lip = Ipsum();
-    String paragraphs = lip.paragraphs(1);
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "About",
-          style: const TextStyle(fontSize: 30),
+          style: TextStyle(fontSize: 30),
         ),
         backgroundColor: const Color(0xFF334150),
         automaticallyImplyLeading: false,
@@ -39,7 +35,7 @@ class About extends StatelessWidget {
         padding:
             const EdgeInsets.only(top: 2.0, right: 4.0, left: 4.0, bottom: 4.0),
         child: Container(
-          padding: EdgeInsets.all(35.0),
+          padding: const EdgeInsets.all(35.0),
           color: const Color.fromARGB(255, 16, 44, 53),
           child: Column(
             children: [
@@ -49,15 +45,16 @@ class About extends StatelessWidget {
                     children: [
                       Center(
                         child: Container(
-                          margin: EdgeInsets.only(top: 30.0, bottom: 50.0),
+                          margin:
+                              const EdgeInsets.only(top: 30.0, bottom: 50.0),
                           child: Image.asset("assets/images/icon.png",
                               width: 120.0),
                         ),
                       ),
-                      Align(
+                      const Align(
                         alignment: Alignment.topLeft,
                         child: Text(
-                          paragraphs,
+                          "This application gives users information about IoT devices in their surroundings like the type of information these devices collect and what privacy options are available. It will show the geolocation of the IoT devices, what type of device it is, what type of data is being collect by the device. The application will not detect the devices by itself, this will be done by the users.",
                           locale: Locale("en", "UK"),
                           textAlign: TextAlign.left,
                           overflow: TextOverflow.visible,
