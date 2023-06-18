cc9de4c1da618e42ea4131537e51bf9c16a1b5d3
Nelson Vieira <2080511@student.uma.pt>
2023-06-18 19:55:39 +0100

    Remove location module from app
    Signed-off-by: Nelson Vieira <2080511@student.uma.pt>

--- masters-thesis/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ masters-thesis/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import firebase_analytics
 import firebase_auth
 import firebase_core
 import geolocator_apple
-import location
 import path_provider_foundation
 import sqflite
 import url_launcher_macos
@@ -23,7 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
